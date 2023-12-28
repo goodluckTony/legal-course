a19a80e5aa15854499ad96d9e3bff63675e354bb
L. Maksym <mlobacov@gmail.com>
2023-12-28 19:21:27 +0200

    test

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -24,13 +24,13 @@ const Footer = () => {
       <div className="top-container">
         <MailForm />
         <div className="title-box">
-          <h2>Lorem ipsum</h2>
-          <p>
+          {/* <h2>Lorem ipsum</h2> */}
+          {/* <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
-          </p>
+          </p> */}
         </div>
       </div>
       <div className="bottom-container">
